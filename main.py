f1dfdbe0107938312f5ab93d5de5fa553be7ed28
realhardik18 <realhardik18@gmail.com>
2022-09-11 14:08:32 +0530

    got command reading from txt file functionality working

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 import discord
 from discord import app_commands
-from creds import BOT_TOKEN, GUILD_ID
-
+from creds import BOT_TOKEN,GUILD_ID
+from commandReader import returnCommands
 
 class aclient(discord.Client):
     def __init__(self):
         super().__init__(intents=discord.Intents.default())
-        self.synced = False
-
+        self.synced=False
     async def on_ready(self):
         if not self.synced:
-            # await tree.sync(guild=discord.Object(id=GUILD_ID))
+            #await tree.sync(guild=discord.Object(id=GUILD_ID))
             await tree.sync()
-            self.synced = True
+            self.synced=True
         print("hello i'm alive")
 
 
-client = aclient()
-tree = app_commands.CommandTree(client)
+client=aclient()
+tree=app_commands.CommandTree(client)
 
-
-@tree.command(name='test', description='test the bot')
-async def self(interaction: discord.Interaction, name: str):
-    await interaction.response.send_message(f'Hello {name}!')
+for command in returnCommands():
+    @tree.command(name=command,description=command)
+    async def self(interaction: discord.Interaction,name:str):
+        await interaction.response.send_message(f'Hello {name}! welcome to {command}')
 
 client.run(BOT_TOKEN)
